f8cee502389f1c1640c18b4be7fce271a604918c
Imtious-Echologyx <imtious@echologyx.com>
2026-09-05 17:34:49 +0600

    Vercel update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
   images: {
+    unoptimized: false,
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: '**.unsplash.com' }
+      { protocol: 'https', hostname: '**.unsplash.com' },
     ],
   },
 }
